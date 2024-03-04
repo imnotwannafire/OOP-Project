b4c0ccb924741ad4537f8bb515d666d3c0768ccd
LocLe-Duc <locducle195@gmail.com>
2024-03-04 21:36:55 +0700

    add inheritance properties

--- inheritaneEmployeeMana.cpp
+++ inheritaneEmployeeMana.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
 #include <string>
-
-class Plumber
+// CLASS PARENT - BASE CLASS
+class Employee
 {
-private:
+protected:
     string name;
     int age;
     string gender;
@@ -12,6 +12,58 @@ private:
     string phone_number;
     float height;
     float weight;
+
+public:
+    void Set();
+    void Get();
+    
+    Employee();
+    ~Employee();
+};
+void Employee::Set()
+{
+    cin.ignore();
+    cout <<"Name: ";
+    getline(cin, name);
+    cout <<"Age: ";
+    cin >> age;
+    cin.ignore();
+    cout <<"Gender: ";
+    getline(cin, gender);
+    cout <<"Address: ";
+    getline(cin, addr);
+    cout <<"Phone number: ";
+    getline(cin, phone_number);
+    cout <<"Height: ";
+    cin >> height;
+    cout <<"Weight: ";
+    cin >> weight;
+}
+
+void Employee::Get()
+{
+    cout <<"\nName: " << name;
+    cout <<"\nAge: " << age;
+    cout <<"\nGender: " << gender;
+    cout <<"\nAddress: " << addr;
+    cout <<"\nPhone Number: " << phone_number;
+    cout <<"\nHeight: " << height;
+    cout <<"\nWeight: " << weight;
+}
+
+Employee::Employee()
+{
+
+}
+Employee::~Employee()
+{
+    
+}
+// CHILDREN CLASS
+class Plumber:public Employee
+{
+private:
+    
     float work_time;
 public:
     void Set();
@@ -31,22 +83,7 @@ Plumber::~Plumber()
 }
 void Plumber::Set()
 {
-    cin.ignore();
-    cout <<"Name: ";
-    getline(cin, name);
-    cout <<"Age: ";
-    cin >> age;
-    cin.ignore();
-    cout <<"Gender: ";
-    getline(cin, gender);
-    cout <<"Address: ";
-    getline(cin, addr);
-    cout <<"Phone number: ";
-    getline(cin, phone_number);
-    cout <<"Height: ";
-    cin >> height;
-    cout <<"Weight: ";
-    cin >> weight;
+    Employee::Set(); // Call parent method
     cout <<"Work Time: ";
     cin >> work_time;
 
@@ -55,13 +92,7 @@ void Plumber::Set()
 
 void Plumber::Get()
 {
-    cout <<"\nName: " << name;
-    cout <<"\nAge: " << age;
-    cout <<"\nGender: " << gender;
-    cout <<"\nAddress: " << addr;
-    cout <<"\nPhone Number: " << phone_number;
-    cout <<"\nHeight: " << height;
-    cout <<"\nWeight: " << weight;
+    Employee::Get();    // Call parent method
     cout <<"\nWork Time: " << work_time;
 }
 
@@ -72,16 +103,9 @@ float Plumber::Cal_salary()
     return sal;
 
 }
-class Delivery
+class Delivery:public Employee
 {
 private:
-    string name;
-    int age;
-    string gender;
-    string addr;
-    string phone_number;
-    float height;
-    float weight;
     int deli_pack;
 public:
     void Set();
@@ -101,22 +125,7 @@ Delivery::~Delivery()
 }
 void Delivery::Set()
 {
-    cin.ignore();
-    cout <<"\nName: ";
-    getline(cin, name);
-    cout <<"\nAge: ";
-    cin >> age;
-    cin.ignore();
-    cout <<"\nGender: ";
-    getline(cin, gender);
-    cout <<"\nAddress: ";
-    getline(cin, addr);
-    cout <<"\nPhone number: ";
-    getline(cin, phone_number);
-    cout <<"\nHeight: ";
-    cin >> height;
-    cout <<"\nWeight: ";
-    cin >> weight;
+    Employee::Set();
     cout <<"\nDelivery Packet: ";
     cin >> deli_pack;
 
@@ -124,13 +133,7 @@ void Delivery::Set()
 
 void Delivery::Get()
 {
-    cout <<"\nName: " << name;
-    cout <<"\nAge: " << age;
-    cout <<"\nGender: " << gender;
-    cout <<"\nAddress: " << addr;
-    cout <<"\nPhone Number: " << phone_number;
-    cout <<"\nHeight: " << height;
-    cout <<"\nWeight: " << weight;
+    Employee::Get();
     cout << "\nDelivered Package: "<<deli_pack;
 }
 
@@ -141,16 +144,10 @@ float Delivery::Cal_salary()
     return sal;
 
 }
-class Driver
+class Driver:public Employee
 {
 private:
-    string name;
-    int age;
-    string gender;
-    string addr;
-    string phone_number;
-    float height;
-    float weight;
+
     float distance;
 public:
     void Set();
@@ -170,22 +167,7 @@ Driver::~Driver()
 }
 void Driver::Set()
 {
-    cin.ignore();
-    cout <<"\nName: ";
-    getline(cin, name);
-    cout <<"\nAge: ";
-    cin >> age;
-    cin.ignore();
-    cout <<"\nGender: ";
-    getline(cin, gender);
-    cout <<"\nAddress: ";
-    getline(cin, addr);
-    cout <<"\nPhone number: ";
-    getline(cin, phone_number);
-    cout <<"\nHeight: ";
-    cin >> height;
-    cout <<"\nWeight: ";
-    cin >> weight;
+    Employee::Set();
     cout <<"\nDistance: ";
     cin >> distance;
 
@@ -194,13 +176,7 @@ void Driver::Set()
 
 void Driver::Get()
 {
-    cout <<"\nName: " << name;
-    cout <<"\nAge: " << age;
-    cout <<"\nGender: " << gender;
-    cout <<"\nAddress: " << addr;
-    cout <<"\nPhone Number: " << phone_number;
-    cout <<"\nHeight: " << height;
-    cout <<"\nWeight: " << weight;
+    Employee::Get();
     cout <<"\nDrive Distance: " << distance;
 }
 float Driver::Cal_salary()
@@ -218,89 +194,95 @@ void pausesys(void)
    } while (cin.get() != '\n');
 
 }
-double PlumberSalarySum(Plumber emp_plumbers[], int m)
+double PlumberSalarySum(Plumber *emp_plumbers[], int m)
 {
     double sum = 0;
     for(int i = 0;i<m;i++)
     {
-        sum += emp_plumbers[i].Cal_salary();
+        sum += emp_plumbers[i]->Cal_salary();
     }
     return sum;
 }
 
-double DeliverySalarySum(Delivery emp_deliverer[], int n)
+double DeliverySalarySum(Delivery *emp_deliverer[], int n)
 {
     double sum = 0;
     for(int i = 0;i<n;i++)
     {
-        sum += emp_deliverer[i].Cal_salary();
+        sum += emp_deliverer[i]->Cal_salary();
     }
     return sum;
 }
 
-double DriverSalarySum(Driver emp_drivers[], int t)
+double DriverSalarySum(Driver *emp_drivers[], int t)
 {
     double sum = 0; 
     for(int i = 0;i<t;i++)
     {
-        sum += emp_drivers[i].Cal_salary();
+        sum += emp_drivers[i]->Cal_salary();
     }
     return sum;
 }
 
 // Swap objects function
-void swap_plumbers(Plumber &plumber1, Plumber &plumber2)
+void swap_plumbers(Plumber *plumber1, Plumber *plumber2)
 {
-    Plumber temp = plumber2;
+    Plumber *temp = new Plumber;
+    temp = plumber2;
     plumber2 = plumber1;
     plumber1 = temp;
+    delete temp;
 }
-void swap_deliverers(Delivery &deli1, Delivery &deli2)
+void swap_deliverers(Delivery *deli1, Delivery *deli2)
 {
-    Delivery temp = deli2;
+    Delivery *temp = new Delivery;
+    temp = deli2;
     deli2 = deli1;
     deli1 = temp;
+    delete temp;
 }
-void swap_drivers(Driver &driver1, Driver &driver2)
+void swap_drivers(Driver *driver1, Driver *driver2)
 {
-    Driver temp = driver2;
+    Driver *temp = new Driver;
+    temp = driver2;
     driver2 = driver1;
     driver1 = temp;
+    delete temp;
 }
 // Sort List by salary
-void sort_plumber(Plumber plumbers[],int n)
+void sort_plumber(Plumber *plumbers[],int n)
 {
     for(int i = 0; i< n-1; i++)
     {
         for(int j = i+1; j<n;j++)
         {
-            if(plumbers[i].Cal_salary()<plumbers[j].Cal_salary())
+            if(plumbers[i]->Cal_salary()<plumbers[j]->Cal_salary())
             {
                 swap_plumbers(plumbers[i], plumbers[j]);
             }
         }
     }
 }
-void sort_deliverer(Delivery deliverers[],int n)
+void sort_deliverer(Delivery *deliverers[],int n)
 {
     for(int i = 0; i< n-1; i++)
     {
         for(int j = i+1; j<n;j++)
         {
-            if(deliverers[i].Cal_salary()<deliverers[j].Cal_salary())
+            if(deliverers[i]->Cal_salary()<deliverers[j]->Cal_salary())
             {
                 swap_deliverers(deliverers[i], deliverers[j]);
             }
         }
     }
 }
-void sort_driver(Driver drivers[],int n)
+void sort_driver(Driver *drivers[],int n)
 {
     for(int i = 0; i< n-1; i++)
     {
         for(int j = i+1; j<n;j++)
         {
-            if(drivers[i].Cal_salary()<drivers[j].Cal_salary())
+            if(drivers[i]->Cal_salary()<drivers[j]->Cal_salary())
             {
                 swap_drivers(drivers[i], drivers[j]);
             }
@@ -308,7 +290,7 @@ void sort_driver(Driver drivers[],int n)
     }
 }
 // Menu application
-void Menu(Plumber emp_plumbers[], Delivery emp_deliverers[], Driver emp_drivers[], int m, int n, int t)
+void Menu(Plumber *emp_plumbers[], Delivery *emp_deliverers[], Driver *emp_drivers[], int m, int n, int t)
 {
     int choice = 0;
     while(true)
@@ -331,21 +313,28 @@ void Menu(Plumber emp_plumbers[], Delivery emp_deliverers[], Driver emp_drivers[
             cout << "\n INVALID CHOICE. PLEASE TRY AGAIN\n";
         }
         else if(choice == 1)
-        {   
+        {
+            Plumber *emp_plumber = new Plumber;   
             cout << "\t\nINPUT PLUMBER EMPLOYEE INFORMATION\n";
-            emp_plumbers[n].Set();
+            emp_plumber->Set();
+            emp_plumbers[n] = emp_plumber;
             n++;
         }
         else if(choice == 2)
-        {   
+        {
+            Delivery *emp_deliverer = new Delivery;   // Allocate object memory
             cout << "\t\nINPUT DELIVERY EMPLOYEE INFORMATION\n";
-            emp_deliverers[m].Set();
+            emp_deliverer->Set();
+            emp_deliverers[m] = emp_deliverer;
             m++;
         }
         else if(choice == 3)
-        {   
+        {
+            Driver *emp_driver = new Driver;   
             cout << "\t\nINPUT DELIVERY EMPLOYEE INFORMATION\n";
-            emp_drivers[t].Set();
+            emp_driver->Set();
+            // Add Pointer object to Arrry of pointer
+            emp_drivers[t] = emp_driver;
             t++;
         }
         else if(choice == 4)
@@ -354,7 +343,7 @@ void Menu(Plumber emp_plumbers[], Delivery emp_deliverers[], Driver emp_drivers[
             for (int i = 0; i< n; i++)
             {
                 cout << "\t\n PLUMBER NUMBER"<<i+1<<endl;
-            emp_plumbers[i].Get();
+            emp_plumbers[i]->Get();
             }
             cout <<"\n\n\t\t TOTAL SALARY FOR PLUMBERS: "<<(size_t)PlumberSalarySum(emp_plumbers,n)<<endl;
             pausesys();
@@ -366,7 +355,7 @@ void Menu(Plumber emp_plumbers[], Delivery emp_deliverers[], Driver emp_drivers[
             for (int i = 0; i< m; i++)
             {
                 cout << "\t\n DELIVERY NUMBER"<<i+1<<endl;
-                emp_deliverers[i].Get();
+                emp_deliverers[i]->Get();
             }
             cout <<"\n\n\t\t TOTAL SALARY FOR DELIVERERS: "<<(size_t)DeliverySalarySum(emp_deliverers,m)<<endl;
             pausesys();
@@ -377,7 +366,7 @@ void Menu(Plumber emp_plumbers[], Delivery emp_deliverers[], Driver emp_drivers[
             for (int i = 0; i< t; i++)
             {
                 cout << "\t\n DRIVER NUMBER"<<i+1<<endl;
-                emp_drivers[i].Get();
+                emp_drivers[i]->Get();
             }
             cout <<"\n\n\t\t TOTAL SALARY FOR DRIVERS: "<<(size_t)DriverSalarySum(emp_drivers,t)<<endl;
             pausesys();
@@ -396,39 +385,57 @@ void Menu(Plumber emp_plumbers[], Delivery emp_deliverers[], Driver emp_drivers[
             for (int i = 0; i< n; i++)
             {
                 cout << "\t\n PLUMBER NUMBER"<<i+1<<endl;
-                emp_plumbers[i].Get();
+                emp_plumbers[i]->Get();
             }
             cout<<"\n\tSORT LIST OF DELIVERER BY SALARY\n";
             sort_deliverer(emp_deliverers,m);
             for (int i = 0; i< m; i++)
             {
                 cout << "\t\n DELIVERS NUMBER"<<i+1<<endl;
-                emp_deliverers[i].Get();
+                emp_deliverers[i]->Set();
             }
             cout<<"\n\tSORT LIST OF DRIVER BY SALARY\n";
             sort_driver(emp_drivers,t);
             for (int i = 0; i< t; i++)
             {
                 cout << "\t\n PLUMBER NUMBER"<<i+1<<endl;
-                emp_drivers[i].Get();
+                emp_drivers[i]->Get();
             }
             
             pausesys();
         }
-        else{
-            cout << "\n\t CHOICE NOT EXISTED.\n";
+        else if(choice == 0){
+            cout << "\n\tEXIT.\n";
+            break;
         }
 
+    }
+    // Delete pointer object for 3 lists
+    for (int i = 0; i< n; i++)
+    {
+        delete emp_plumbers[i];
+    }
+    for (int i = 0; i< m; i++)
+    {
+        delete emp_deliverers[i];
     } 
+
+    for (int i = 0; i< t; i++)
+    {
+        delete emp_drivers[i];
+    } 
+
 
 }
 
 
 int main()
 {
-    Plumber emp_plumbers[100];      //Mang 1 chieu danh sach nhan vien sua ong nuoc
-    Delivery emp_deliverers[100];   //Mang 1 chieu danh sach nhan vien giao hang
-    Driver emp_drivers[100];        //Mang 1 chieu danh sach nhan vien lai xe
+    // Pointer Object - declare list of 100 pointers
+
+    Plumber *emp_plumbers[100];      //Mang 1 chieu pointer nhan vien sua ong nuoc
+    Delivery *emp_deliverers[100];   //Mang 1 chieu pointer nhan vien giao hang
+    Driver *emp_drivers[100];        //Mang 1 chieu pointer nhan vien lai xe
     int m = 0;
     int n = 0;
     int t = 0;
